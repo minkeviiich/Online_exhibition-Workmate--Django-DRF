bbee6b2c004257659bce0a4399a138aaab6c848d
Alexander <minkeviiich@gmail.com>
2024-09-30 22:28:08 +0300

    second commit.1

--- workmate/kittens/serializers.py
+++ workmate/kittens/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Kitten, Breed, Rating, CustomUser
 
+
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
